90c97a0f9f3e9bd8a4e79f7292f3c4918249c1b3
Tao Wu <Tao.Wu@microchip.com>
2024-06-07 15:17:26 +0800

    test restic deploy on local

--- script/restic_deploy_ssh.sh
+++ script/restic_deploy_ssh.sh
@@ -5,3 +5,25 @@ configFile="$(dirname $scriptDir)"/vault/machine.txt
 
 source $configFile
 export IPV4_ADDR=$IPV4_ADDR
+
+resticShSource="$(dirname $scriptDir)"/restic/restic.sh
+resticInSource="$(dirname $scriptDir)"/restic/restic_includes.txt
+resticExSource="$(dirname $scriptDir)"/restic/restic_excludes.txt
+resticEnvSource="$(dirname $scriptDir)"/vault/restic_env.txt
+
+#targetLocation="/root/restic"
+targetLocation="."
+resticShDestination="$targetLocation"/restic.sh
+resticInDestination="$targetLocation"/restic_includes.txt
+resticExDestination="$targetLocation"/restic_excludes.txt
+resticEnvDestination="$targetLocation"/restic_env.txt
+
+resticShContent="$(cat $resticShSource)"
+resticInContent="$(cat $resticInSource)"
+resticExContent="$(cat $resticExSource)"
+resticEnvContent="$(cat $resticEnvSource)"
+
+echo "$resticShContent" > "$resticShDestination"
+echo "$resticInContent" > "$resticInDestination"
+echo "$resticExContent" > "$resticExDestination"
+echo "$resticEnvContent" > "$resticEnvDestination"
