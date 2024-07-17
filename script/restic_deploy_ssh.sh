3cd26ec582df3948e8611efb607815612f2e77c2
Tao Wu <Tao.Wu@microchip.com>
2024-07-17 11:51:52 +0800

    need to test restic_deploy

--- script/restic_deploy_ssh.sh
+++ script/restic_deploy_ssh.sh
@@ -6,31 +6,8 @@ resticShSource="$(dirname $scriptDir)"/restic/restic.sh
 resticInSource="$(dirname $scriptDir)"/restic/restic_includes.txt
 resticExSource="$(dirname $scriptDir)"/restic/restic_excludes.txt
 resticEnvSource="$(dirname $scriptDir)"/vault/restic.env
+resticSource="$resticShSource $resticInSource $resticExSource $resticEnvSource"
 
-#targetLocation="."
-#targetLocation="/root/restic"
-targetLocation="$(dirname $scriptDir)"/test
-resticShDestination="$targetLocation"/restic.sh
-resticInDestination="$targetLocation"/restic_includes.txt
-resticExDestination="$targetLocation"/restic_excludes.txt
-resticEnvDestination="$targetLocation"/restic.env
-
-resticShContent="$(cat $resticShSource)"
-resticInContent="$(cat $resticInSource)"
-resticExContent="$(cat $resticExSource)"
-resticEnvContent="$(cat $resticEnvSource)"
-
-# TODO: check target directory exist
-sshCMD="$(cat << sshCMDEnd
-echo "$resticShContent" > "$resticShDestination";
-echo "$resticInContent" > "$resticInDestination";
-echo "$resticExContent" > "$resticExDestination";
-echo "$resticEnvContent" > "$resticEnvDestination";
-sshCMDEnd
-)"
-
-configFile="$(dirname $scriptDir)"/vault/machine.txt
-source $configFile
-
-ssh "$SSH_USER"@"$IPV4_ADDR" "$sshCMD"
+targetLocation="/root/restic"
+scp -r "$resticSource" "$SSH_USER"@"$IPV4_ADDR":"$targetLocation"
 echo done
