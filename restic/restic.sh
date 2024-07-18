cebf9e75cc58b48d762e5c281e94eeee772a8179
Tao Wu <Tao.Wu@microchip.com>
2024-07-18 15:17:20 +0800

    restic.sh support to decrypt env data

--- restic/restic.sh
+++ restic/restic.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-#!/bin/bash
-
 # restic_test
 #restic -r /root/tmp/restic_test --verbose backup --files-from /root/restic/restic_includes.txt --exclude-file /root/restic/restic_excludes.txt
 
-# restic
+# restic config directory
 resticDir="$(dirname $0)"
-envFile="$resticDir"/restic.env
+envEnc="$resticDir"/restic_env.enc
 
-source $envFile
-export B2_ACCOUNT_ID=$B2_ACCOUNT_ID
-export B2_ACCOUNT_KEY=$B2_ACCOUNT_KEY
+# b2 account info
+accountID=
+accountKEY=
+envString="$(gpg --decrypt $envFile)"
+source <(echo "$envFile")
+accountID=$B2_ACCOUNT_ID
+accountKEY=$B2_ACCOUNT_KEY
+if [ -z "$accountID"]; then
+	echo "B2 account ID is empty"
+	exit 1
+fi
+if [ -z "$accountKEY"]; then
+	echo "B2 account KEY is empty"
+	exit 1
+fi
 
+# export b2 environment
+export B2_ACCOUNT_ID=$ACCOUNT_ID
+export B2_ACCOUNT_KEY=$ACCOUNT_KEY
+
+# restic
 restic -r b2:greencloud:restic_backup --verbose backup --files-from /root/restic/restic_includes.txt --exclude-file /root/restic/restic_excludes.txt
