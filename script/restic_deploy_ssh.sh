b648510c96fc10fb029cf25897058bdb5a7aeb88
Tao Wu <Tao.Wu@microchip.com>
2024-07-24 11:16:45 +0800

    modify scp to send multiple files

--- script/restic_deploy_ssh.sh
+++ script/restic_deploy_ssh.sh
@@ -6,8 +6,21 @@ resticShSource="$(dirname $scriptDir)"/restic/restic.sh
 resticInSource="$(dirname $scriptDir)"/restic/restic_includes.txt
 resticExSource="$(dirname $scriptDir)"/restic/restic_excludes.txt
 resticEnvSource="$(dirname $scriptDir)"/vault/restic.env
-resticSource="$resticShSource $resticInSource $resticExSource $resticEnvSource"
+resticSource="{$resticShSource, $resticInSource, $resticExSource, $resticEnvSource}"
 
 targetLocation="/root/restic"
-scp -r "$resticSource" "$SSH_USER"@"$IPV4_ADDR":"$targetLocation"
+scp "$resticSource" "$SSH_USER"@"$IPV4_ADDR":"$targetLocation"
 echo done
+
+#resticTmpDir="$(dirname $scriptDir)"/resticTmp
+#mkdir "$resticTmpDir"
+
+#cp "$resticShSource" "$resticTmpDir"
+#cp "$resticInSource" "$resticTmpDir"
+#cp "$resticExSource" "$resticTmpDir"
+#cp "$resticEnvSource" "$resticTmpDir"
+
+#scp -r "$resticTmpdir"/* "$SSH_USER"@"$IPV4_ADDR":"$targetLocation"
+
+#rm -drf "$resticTmpDir"
+#echo done
