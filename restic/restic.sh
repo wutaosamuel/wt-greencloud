a9d9ad71a74138d6e288726c6026ecc160e1ae52
wutao <497863986@qq.com>
2024-07-26 22:32:05 +0800

    need to fix restic with wrong b2 account

--- restic/restic.sh
+++ restic/restic.sh
@@ -10,22 +10,29 @@ envEnc="$resticDir"/restic_env.enc
 # b2 account info
 accountID=
 accountKEY=
-envString="$(gpg --decrypt $envFile)"
-source <(echo "$envFile")
-accountID=$B2_ACCOUNT_ID
-accountKEY=$B2_ACCOUNT_KEY
-if [ -z "$accountID"]; then
+passphrase=""
+#read -s -p "Enter passphrase for b2 account: " passphrase
+#echo ""
+#envString="$(gpg --passphrase $passphrase --decrypt $envEnc)"
+envString="$(gpg --decrypt $envEnc)"
+echo "$envString"
+source <(echo "$envString")
+accountID="$B2_ACCOUNT_ID"
+accountKEY="$B2_ACCOUNT_KEY"
+if [ -z "$accountID" ]; then
 	echo "B2 account ID is empty"
 	exit 1
 fi
-if [ -z "$accountKEY"]; then
+if [ -z "$accountKEY" ]; then
 	echo "B2 account KEY is empty"
 	exit 1
 fi
+echo "B2_ACCOUNT_ID=$accountID"
+echo "B2_ACCOUNT_KEY=$accountKEY"
 
 # export b2 environment
-export B2_ACCOUNT_ID=$ACCOUNT_ID
-export B2_ACCOUNT_KEY=$ACCOUNT_KEY
+export B2_ACCOUNT_ID="$accountID"
+export B2_ACCOUNT_KEY="$accountKEY"
 
 # restic
-restic -r b2:greencloud:restic_backup --verbose backup --files-from /root/restic/restic_includes.txt --exclude-file /root/restic/restic_excludes.txt
+restic -r b2:greencloud:restic_backup --verbose backup --files-from "$resticDir"/restic_includes.txt --exclude-file "$resticDir"/restic_excludes.txt
--- script/restic_deploy_ssh.sh
+++ script/restic_deploy_ssh.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 scriptDir="$(dirname $0)"
+machine="$(dirname $scriptDir)"/vault/machine.txt
+source "$machine"
 
 resticShSource="$(dirname $scriptDir)"/restic/restic.sh
 resticInSource="$(dirname $scriptDir)"/restic/restic_includes.txt
@@ -9,18 +11,19 @@ resticEnvSource="$(dirname $scriptDir)"/vault/restic_env.enc
 resticSource="$resticShSource $resticInSource $resticExSource $resticEnvSource"
 
 targetLocation="/root/restic"
-scp "$resticSource" "$SSH_USER"@"$IPV4_ADDR":"$targetLocation"
-echo done
-
-#resticTmpDir="$(dirname $scriptDir)"/resticTmp
-#mkdir "$resticTmpDir"
-
-#cp "$resticShSource" "$resticTmpDir"
-#cp "$resticInSource" "$resticTmpDir"
-#cp "$resticExSource" "$resticTmpDir"
-#cp "$resticEnvSource" "$resticTmpDir"
-
-#scp -r "$resticTmpdir"/* "$SSH_USER"@"$IPV4_ADDR":"$targetLocation"
-
-#rm -drf "$resticTmpDir"
+#scp "$resticSource" "$SSH_USER"@"$IPV4_ADDR":"$targetLocation"
 #echo done
+
+resticTmpDir="$(dirname $scriptDir)"/resticTmp
+mkdir "$resticTmpDir"
+
+cp "$resticShSource" "$resticTmpDir"
+cp "$resticInSource" "$resticTmpDir"
+cp "$resticExSource" "$resticTmpDir"
+cp "$resticEnvSource" "$resticTmpDir"
+
+scp -r "$resticTmpDir"/* "$SSH_USER"@"$IPV4_ADDR":"$targetLocation"
+#scp -r "$resticTmpDir"/* "$SSH_USER"@172.16.63.62:"$targetLocation"
+
+rm -drf "$resticTmpDir"
+echo done
